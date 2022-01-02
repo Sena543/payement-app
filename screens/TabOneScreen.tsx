afb865a5726c793a88f44cba87acb69dc9dc9cc7
Sena543 <senanuazanu@gmail.com>
2022-01-02 23:43:13 +0000

    Initial commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -7,7 +7,7 @@ import { RootTabScreenProps } from "../types";
 export default function TabOneScreen({ navigation }: RootTabScreenProps<"TabOne">) {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.title}>Hello World</Text>
+			<Text style={styles.title}>This is a test for the home screen.</Text>
 			<View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
 			<EditScreenInfo path="/screens/TabOneScreen.tsx" />
 		</View>
